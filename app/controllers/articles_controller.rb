7b81fae2e71d53702eab15be51f05003911c6744
Alex Babenko <bob78alex@gmail.com>
2018-05-31 03:23:51 +0000

    Troubleshooted version, edition 1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,24 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
     def new
         @article = Article.new
     end
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+     @article = Article.find(params[:id])
+     if @article.update(article_params)
+        flash[:notice] = "Article was successfully updated"
+        redirect_to article_path(@article)
+        else
+            render 'edit'
+     end
+    end
+     
+    
     def article_params
         params.require(:article).permit(:title, :description)
     end
@@ -15,8 +32,14 @@ class ArticlesController < ApplicationController
        else
            render 'new'
     end
+    end
     def show
         @article = Article.find(params[:id])
     end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice] = "Article deleted"
+        redirect_to articles_path
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-    #validates :title, presence: true, length: {minimum: 3, maximum: 50}
-   # validates :description, presence: true, legth: {minimum: 5, maximum: 200}
+  validates :title, presence: true, length: {minimum: 3, maximum: 50}
+  validates :description, presence: true, length: {minimum: 5, maximum: 200}
     
 end
