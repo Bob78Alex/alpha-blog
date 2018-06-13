001907599b6f3f22479de8581ec434d7de0fdb1e
Alex Babenko <bob78alex@gmail.com>
2018-06-13 00:27:16 +0000

    User view and articles personalized

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,21 @@ class UsersController < ApplicationController
             render 'new'
         end
     end
+    def edit
+        @user = User.find(params[:id])
+    end
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Your account was successfully updated"
+            redirect_to article_path
+        else
+            render 'edit'
+        end
+    end
+    def show
+        @user = User.find(params[:id])
+    end 
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
