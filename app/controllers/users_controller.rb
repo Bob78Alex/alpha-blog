850900b0b825094888248a734f1609be5c0e093a
Alex Babenko <bob78alex@gmail.com>
2018-06-16 21:05:01 +0000

    Adding login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    
+    before_action :set_user, only[:edit, :update, :show]
     def index
         @users = User.paginate(page: params[:page], per_page: 3)
     end
@@ -18,10 +18,10 @@ class UsersController < ApplicationController
         end
     end
     def edit
-        @user = User.find(params[:id])
+       
     end
     def update
-        @user = User.find(params[:id])
+        
         if @user.update(user_params)
             flash[:success] = "Your account was successfully updated"
             redirect_to article_path
@@ -30,12 +30,15 @@ class UsersController < ApplicationController
         end
     end
     def show
-        @user = User.find(params[:id])
+        
         @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
     end 
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+    def set_user
+        @user = User.find(params[:id])
     end
 end
 
